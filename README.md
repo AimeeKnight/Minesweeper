e563c4a3aab34e9077518f146872f451956ca696
AimeeKnight <aimee.knight@messagesystems.com>
2016-01-18 21:38:38 -0500

    render basic component to display board

---
+++ README.md
@@ -0,0 +1,5 @@
+run `npm install`
+
+followed by `node server.js`
+
+navigate to [localhost:8080](http://localhost:8080)
---
+++ minesweeper.js
@@ -0,0 +1,10 @@
+var Minesweeper = React.createClass({
+  render: function() {
+    return <div>Minesweeper Board</div>;
+  }
+});
+
+ReactDOM.render(
+  <Minesweeper/>,
+  document.getElementById('container')
+);
