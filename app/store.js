7ca7c6ef37822d93d6d73309066e2fb2e02e97ff
AimeeKnight <aimeemarieknight@gmail.com>
2016-03-23 09:49:02 -0400

    remove ability to set exisiting state tile as exposed

--- app/store.js
+++ app/store.js
@@ -1,12 +1,15 @@
 import {createStore} from 'redux'
 import initialBoard from './helpers'
+import Tile from './tile'
 
 const DEFAULTSTATE = initialBoard(9, 9)
 
 const board = (state = DEFAULTSTATE, action) => {
   switch (action.type) {
     case 'CLICK':
-      action.tile.markAsExposed()
+      let newTile = new Tile(tile.id)
+      newTile.markAsExposed()
+      //TODO insert new tile into existing slot
       return state
     default:
       return state
